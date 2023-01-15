ac556b18eee4cb83c192f07b1999d537f42fe7bf
Navruzbek2103 <navrozbek2306@gmail.com>
2023-01-15 05:28:10 +0500

    Edit

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
 let BtnGroup = document.querySelector(".wrapper-btn-group")
-// let Btns = document.querySelectorAll(".btns")
 let monitor = document.querySelector(".wrapper-calculator-monitor")
-
-
 let viewChar = []
 let toStringElements = ""
 let firsValue = 0;
@@ -11,24 +8,16 @@ let plus = 0;
 let minus = 0;
 let multiplication = 0;
 let division = 0;
-let result2 = 0;
+let result = 0;
 BtnGroup.addEventListener("click", (e) => {
   if (e.target.classList.contains("btns")) {
-
-    // if(viewChar.charAt(0))
-    // console.log(toStringElements);
-
     if (e.target.textContent === "Del" || e.target.textContent === "Reset") {
       viewChar = []
       monitor.textContent = 0
-
     }
-
     else {
       viewChar.push(e.target.textContent)
-
       toStringElements = viewChar.join("")
-
       if (toStringElements.charAt(0) === "+" || toStringElements.charAt(0) === "-" || toStringElements.charAt(0) === "x" || toStringElements.charAt(0) === "/" || (toStringElements.charAt(0) === "0" && toStringElements.charAt(1) !== ".")) {
         viewChar = []
       }
@@ -36,55 +25,40 @@ BtnGroup.addEventListener("click", (e) => {
         monitor.textContent = "0" + toStringElements
       }
       else if (e.target.textContent === "=") {
-        monitor.textContent = result2
-        // console.log(result2);
-        viewChar = [result2]
+        monitor.textContent = result
+        viewChar = [result]
       }
       else {
         monitor.textContent = toStringElements
-
         // operator plus
         if (toStringElements.includes("+")) {
           plus = toStringElements.split("+")
-
           firsValue = plus[0] * 1
           secondValue = plus[1] * 1
-          result2 = firsValue + secondValue
+          result = firsValue + secondValue
         }
-
         // operator minus
         else if (toStringElements.includes("-")) {
           minus = toStringElements.split("-")
-
           firsValue = minus[0] * 1
           secondValue = minus[1] * 1
-          result2 = firsValue - secondValue
+          result = firsValue - secondValue
         }
-
         // operator multiplication
         else if (toStringElements.includes("x")) {
           multiplication = toStringElements.split("x")
-          console.log(toStringElements);
-
           firsValue = multiplication[0] * 1
           secondValue = multiplication[1] * 1
-          result2 = firsValue * secondValue
+          result = firsValue * secondValue
         }
-
         // operator division
         else if (toStringElements.includes("/")) {
           division = toStringElements.split("/")
-
           firsValue = division[0] * 1
           secondValue = division[1] * 1
-          result2 = firsValue / secondValue
+          result = firsValue / secondValue
         }
-
-
       }
-
-
     }
   }
-
 })
